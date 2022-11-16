8e7820cddd0d40042aa8380cc896786b328bb35a
Mounir-mk <mounir.mekoui@gmail.com>
2022-11-16 19:12:42 +0100

    reverting to old commit

--- frontend/src/components/cardPropTypes.js
+++ frontend/src/components/cardPropTypes.js
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 
 const cardPropTypes = PropTypes.shape({
-    id: PropTypes.string,
-    name: PropTypes.string,
-    powerstats: PropTypes.shape({
-        intelligence: PropTypes.string,
-        strength: PropTypes.string,
-        speed: PropTypes.string,
-        durability: PropTypes.string,
-        power: PropTypes.string,
-        combat: PropTypes.string,
-    }),
-    image: PropTypes.PropTypes.shape({ url: PropTypes.string }),
+  id: PropTypes.string,
+  name: PropTypes.string,
+  powerstats: PropTypes.shape({
+    intelligence: PropTypes.string,
+    strength: PropTypes.string,
+    speed: PropTypes.string,
+    durability: PropTypes.string,
+    power: PropTypes.string,
+    combat: PropTypes.string,
+  }),
+  image: PropTypes.PropTypes.shape({ url: PropTypes.string }),
 });
 
 const cardArrayPropTypes = PropTypes.arrayOf(cardPropTypes);
