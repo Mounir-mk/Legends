8393b4b6685348331512f3e0a12570075a7491ac
Mounir <85800425+Mounir-mk@users.noreply.github.com>
2022-11-16 17:06:32 +0100

    no more duplicating heros

--- frontend/src/components/Draft/DraftPage.jsx
+++ frontend/src/components/Draft/DraftPage.jsx
@@ -17,16 +17,15 @@ export default function DraftPage({
 
   const [isLoading, setLoading] = useState(false);
   const API_KEY = "4070424506516370";
-  const [validIds, setValidIds] = useState([
+  const validIds = [
     644, 720, 306, 491, 165, 156, 267, 542, 405, 370, 284, 601, 309, 522, 731,
     105, 609, 346, 659, 332, 620, 149, 717, 106, 498, 273, 430, 414, 401, 687,
     550, 570, 472, 655, 278, 708, 136, 230, 457, 70, 38, 95, 31, 93,
-  ]);
+  ].sort(() => Math.random() - 0.5);
 
   const fetchData = () => {
     const promises = validIds
-      .sort(() => Math.random() - 0.5)
-      .splice(0, 3)
+      .slice(draftRound * 3, (draftRound + 1) * 3)
       .map((id) =>
         axios.get(`https://www.superheroapi.com/api.php/${API_KEY}/${id}`)
       );
@@ -56,7 +55,6 @@ export default function DraftPage({
       setCharacters={setCharacters}
       draftRound={draftRound}
       setDraftRound={setDraftRound}
-      setValidIds={setValidIds}
     />
   );
 }
